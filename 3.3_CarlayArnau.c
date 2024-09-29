89470cee84459c8c8ce953f02606976a993fe2c5
carlalopezolmedo <carla.lopez.olmedo@estudiantat.upc.edu>
2024-09-29 23:48:12 +0200

    Add files via upload

---
+++ 3.3_CarlayArnau.c
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+
+DROP DATABASE IF EXISTS JuegoPropio;
+CREATE DATABASE JuegoPropio;
+USE JuegoPropio;
+
+CREATE TABLE Jugador (
+id INTEGER PRIMARY KEY NOT NULL,
+Nombre TEXT NOT NULL
+)ENGINE = InnoDB;
+
+
+CREATE TABLE MedallasObtenidas (
+Jugador INTEGER NOT NULL,
+Medallas INTEGER NOT NULL,
+FOREIGN KEY (Jugador) REFERENCES Jugador(id),
+)ENGINE = InnoDB;
+
+
+INSERT INTO Jugador VALUES(1,'Carla');
+INSERT INTO Jugador VALUES(2,'Aaron');
+INSERT INTO Jugador VALUES(3,'Alejandro');
+INSERT INTO Jugador VALUES(4,'Arnau');
+
+INSERT INTO MedallasObtenidas VALUES(1, 20);
+INSERT INTO MedallasObtenidas VALUES(2, 40);
+INSERT INTO MedallasObtenidas VALUES(3, 5);
+INSERT INTO MedallasObtenidas VALUES(4, 29);
+
